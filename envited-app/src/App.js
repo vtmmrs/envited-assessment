784a8348d77c10299eccaa78e902cd359f2cb8b9
Vivien Timmers <djyak.be@gmail.com>
2022-02-21 12:02:11 +1100

    init

---
+++ envited-app/src/App.js
@@ -0,0 +1,9 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Header from './components/Header'
+
+function App() {
+  return
+}
+
+export default App;
---
+++ envited-app/src/components/header.js
@@ -0,0 +1,15 @@
+// import PropTypes from 'prop-types'
+
+// const Header = ({ title }) => {
+//   return (
+//   <header>
+//     <h1 style={headingStyle}>{style}</h1>
+//   </header>
+//   )
+// }
+
+// const headingStyle = {
+//   color: 'white'
+// }
+
+// export default Header
